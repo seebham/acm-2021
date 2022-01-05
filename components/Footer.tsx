7ef082ba30f62fbec12dee56cf3a66d10db18719
seebham <shubhamisdev@gmail.com>
2022-01-05 21:33:21 +0530

    feat: :sparkles: Nav Links Update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,79 +6,63 @@ const Footer = () => {
       <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
         <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">YCCE ACM Student Chapter</span>
+            <span className="text-xl">YCCE ACM Student Chapter</span>
           </a>
           <p className="mt-2 text-sm text-gray-500">
-            Air plant banjo lyft occupy retro adaptogen indego
+            ACM Student Chapter of YCCE, Nagpur
           </p>
         </div>
         <div className="flex-grow flex flex-wrap justify-end md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+              Official Links
             </h2>
             <nav className="list-none mb-10">
               <li>
-                <a className="link">First Link</a>
+                <a
+                  className="link"
+                  href="https://ycce.edu/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  YCCE
+                </a>
               </li>
               <li>
-                <a className="link">Second Link</a>
+                <a
+                  className="link"
+                  href="https://india.acm.org/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  ACM India
+                </a>
               </li>
               <li>
-                <a className="link">Third Link</a>
-              </li>
-              <li>
-                <a className="link">Fourth Link</a>
+                <a
+                  className="link"
+                  href="https://dl.acm.org/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  ACM Digital Library
+                </a>
               </li>
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+              Sections
             </h2>
             <nav className="list-none mb-10">
               <li>
-                <a className="link">First Link</a>
+                <a className="link">Events</a>
               </li>
               <li>
-                <a className="link">Second Link</a>
+                <a className="link">About</a>
               </li>
               <li>
-                <a className="link">Third Link</a>
-              </li>
-              <li>
-                <a className="link">Fourth Link</a>
-              </li>
-            </nav>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
-            </h2>
-            <nav className="list-none mb-10">
-              <li>
-                <a className="link">First Link</a>
-              </li>
-              <li>
-                <a className="link">Second Link</a>
-              </li>
-              <li>
-                <a className="link">Third Link</a>
-              </li>
-              <li>
-                <a className="link">Fourth Link</a>
+                <a className="link">Team</a>
               </li>
             </nav>
           </div>
