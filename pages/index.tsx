ef177b2079bf30742dbadf2e40d97e2e10d4c528
seebham <shubhamisdev@gmail.com>
2022-01-05 15:10:02 +0530

    feat: :sparkles: Header added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="text-gray-600 body-font">
       <Head>
         <title>YCCE ACM Student Chapter</title>
         <meta name="description" content="ACM Student Chapter Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
       <main>
         <div className="text-3xl">ACM Student Chapter - YCCE</div>
       </main>
